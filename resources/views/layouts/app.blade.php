863d2570f4f7bd0eb8138479caf1dd063ddc56ed
kei.awa <ogita2siam.git@gmail.com>
2020-02-22 06:50:45 +0000

    top page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,25 +8,15 @@
     </head>
     
     <body>
-        <header class="mb-4">
-            <nav class="navbar bavbar-expand-sm navbar-light bg-white boder-bottom" >
-                <a class="navbar-brand" href="/">Tasklist</a>
-                
-                <div class="collapse navbar-collapse" id="nav-bar">
-                    <ul class='navbar-nav mr-auto'></ul>
-                    <ul class="navbar-nav">
-                        <li class='nav-bar'>{!! link_to_route('tasks.create', '新規タスクの追加 ', [], ['class' => 'btn btn-primary']) !!}</li>
-                    </ul>
-                </div> 
-            </nav> 
-        </header>
         
-            <div class="container">
+        @include('commons.navbar')
+        
+        <div class="container">
                 
-                @include('commons.error_messages')
+        @include('commons.error_messages')
                 
-                @yield('content')
-            </div>
+        @yield('content')
+        </div>
             
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
